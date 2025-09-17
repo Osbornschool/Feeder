2ceff3234ac9321bb4225877b65c6af600a0e42d
OsbornChen <osbornc@nycstudents.net>
2025-09-17 11:44:12 -0400

    4th final commit

--- Main.java
+++ Main.java
@@ -1,12 +1,20 @@
+
 public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder(1000);
+        Feeder f = new Feeder(500);
         System.out.println(f);
-        System.out.println(f.getCurrentFood());
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f.simulateOneDay(12);
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f = new Feeder(1000);
         f.simulateOneDay(22);
-        System.out.println(f.getCurrentFood());
-        System.out.println((int) (Math.random() * 50));
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f = new Feeder(2400);
+        System.out.println(f.simulateManyDays(10, 4) + " days");
+
+
+
     }
 }
